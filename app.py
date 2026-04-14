b5ae6fa60ec7cab6f0fb07f60531880ec4576163
TheBK2026 <beshoymagdy090@gmail.com>
2026-04-14 18:45:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -203,8 +203,15 @@ def mark_attendance():
 # ----------------------------
 # RUN
 # ----------------------------
-if __name__ == '__main__':
-   import os
 
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False,   # IMPORTANT for Render
+        use_reloader=False
+    )
